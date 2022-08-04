41cf002fb39c83edf0f144fc25f1f1a29ac2cab2
balakumaran <balakumaran247@gmail.com>
2022-08-04 21:14:20 +0530

    added: download_grd method

--- imdgpmlib/core.py
+++ imdgpmlib/core.py
@@ -1,6 +1,11 @@
-class IMDGPM:
+import requests, os
+from datetime import datetime
+from typing import Optional
 
-    __URL = {
+
+class IMD:
+
+    __IMDURL = {
         "rain": "https://www.imdpune.gov.in/Seasons/Temperature/gpm/",
         "tmax": "https://www.imdpune.gov.in/Seasons/Temperature/max/",
         "tmin": "https://www.imdpune.gov.in/Seasons/Temperature/min/",
@@ -19,15 +24,24 @@ class IMDGPM:
 
     def __init__(self, var_type: str) -> None:
         self.var_type = var_type
-        self.__url = self.__URL[self.var_type]
-        self.__prefix, self.__dtfmt = self.__IMDFILEFMT[self.var_type]
+        self.__imdurl = IMD.__IMDURL[self.var_type]
+        self.__pfx, self.__dtfmt = IMD.__IMDFILEFMT[self.var_type]
 
-    def download_grd(self):
-        pass
+    def download_grd(self, dt: datetime, path: str) -> Optional[str]:
+        url = f"{self.__imdurl}{self.__pfx}{dt.strftime(self.__dtfmt)}.grd"
+        filename = f"{dt.strftime('%Y%m%d')}.grd"
+        out_file = os.path.join(path, filename)
+        r = requests.get(url, allow_redirects=True)
+        if os.path.exists(out_file) or r.status_code != 200:
+            return filename
+        with open(out_file, "wb") as f:
+            f.write(r.content)
+        return None
 
-class MultiDateData(IMDGPM):
-    """get the start date and end date as datetime and path
-    loop through the dates and call download_grd
-    and assign download_failed
-    also check if file aready exist and assign skipped"""
-    pass
+
+# class MultiDateData(IMD):
+#     """get the start date and end date as datetime and path
+#     loop through the dates and call download_grd
+#     and assign download_failed
+#     also check if file aready exist and assign skipped"""
+#     pass

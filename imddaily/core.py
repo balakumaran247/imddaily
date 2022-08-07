30e1371a664354e140f0f7afa65b29a801e445cb
balakumaran <balakumaran247@gmail.com>
2022-08-07 23:50:08 +0530

    added: download checks

--- imddaily/core.py
+++ imddaily/core.py
@@ -6,37 +6,62 @@ from typing import Optional
 class IMD:
 
     __IMDURL = {
-        "rain": "https://www.imdpune.gov.in/Seasons/Temperature/gpm/",
+        "raingpm": "https://www.imdpune.gov.in/Seasons/Temperature/gpm/",
         "tmax": "https://www.imdpune.gov.in/Seasons/Temperature/max/",
         "tmin": "https://www.imdpune.gov.in/Seasons/Temperature/min/",
-        "raingrd": "https://www.imdpune.gov.in/Seasons/Temperature/Rainfall/",
+        "rain": "https://www.imdpune.gov.in/Seasons/Temperature/Rainfall/",
         "tmaxone": "https://www.imdpune.gov.in/Seasons/Temperature/max/",
         "tminone": "https://www.imdpune.gov.in/Seasons/Temperature/min/",
     }  # https://www.imdpune.gov.in/Seasons/Temperature/temp.html
-    __IMDFILEFMT = {
-        "rain": ("", "%d%m%Y"),
-        "tmax": ("max", "%d%m%Y"),
-        "tmin": ("min", "%d%m%Y"),
-        "raingrd": ("rain_ind0.25_", "%y_%m_%d"),
-        "tmaxone": ("max1_", "%d%m%Y"),
-        "tminone": ("min1_", "%d%m%Y"),
+    __IMDFMT = {
+        "raingpm": ("", "%d%m%Y", "raingpm_"),
+        "tmax": ("max", "%d%m%Y", "tmax_"),
+        "tmin": ("min", "%d%m%Y", "tmin_"),
+        "rain": ("rain_ind0.25_", "%y_%m_%d", "rain_"),
+        "tmaxone": ("max1_", "%d%m%Y", "tmaxone_"),
+        "tminone": ("min1_", "%d%m%Y", "tminone_"),
     }
 
     def __init__(self, var_type: str) -> None:
         self.var_type = var_type
         self.__imdurl = IMD.__IMDURL[self.var_type]
-        self.__pfx, self.__dtfmt = IMD.__IMDFILEFMT[self.var_type]
+        self.__pfx, self.__dtfmt, self.__opfx = IMD.__IMDFMT[self.var_type]
 
-    def download_grd(self, dt: datetime, path: str) -> Optional[str]:
+    def _download_grd(self, dt: datetime, path: str) -> Optional[str]:
         url = f"{self.__imdurl}{self.__pfx}{dt.strftime(self.__dtfmt)}.grd"
-        filename = f"{dt.strftime('%Y%m%d')}.grd"
+        filename = f"{self.__opfx}{dt.strftime('%Y%m%d')}.grd"
         out_file = os.path.join(path, filename)
+        if os.path.exists(out_file):
+            return filename
         r = requests.get(url, allow_redirects=True)
-        if os.path.exists(out_file) or r.status_code != 200:
+        if r.status_code != 200:
             return filename
         with open(out_file, "wb") as f:
             f.write(r.content)
         return None
+
+    def _check_path(self, path: str) -> str:
+        check_path = os.path.normpath(path)
+        if not os.path.isdir(check_path):
+            raise OSError(f"{check_path} does not exist.")
+        return check_path
+
+    def _check_dates(self, start_date: datetime, end_date: datetime) -> datetime:
+        self.__imd_first_date = {
+            "raingpm": datetime.strptime('20151001', '%Y%m%d'),
+            "tmax": datetime.strptime('20150601','%Y%m%d'),
+            "tmin": datetime.strptime('20150601','%Y%m%d'),
+            "rain": datetime.strptime('20181209', '%Y%m%d'),
+            "tmaxone": datetime.strptime('20190101','%Y%m%d'),
+            "tminone": datetime.strptime('20190101','%Y%m%d'),
+        }
+        self.__first_date = self.__imd_first_date[self.var_type]
+        if start_date > end_date:
+            start_date, end_date = end_date, start_date
+        if self.__first_date > start_date:
+            raise ValueError(
+                f'Data available after {self.__first_date.strftime("%Y-%m-%d")}')
+        return start_date, end_date
 
 
 # class MultiDateData(IMD):
--- imddaily/imddaily.py
+++ imddaily/imddaily.py
@@ -1,28 +1,27 @@
-from re import I
 from core import IMD
 from datetime import datetime
 import os
 
 
 class get_data:
-    def __init__(self, type: str, start_date: str, end_date: str, path: str) -> None:
-        self.var_type = type
-        self.__imd_obj = IMD(type)
-        self.start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        self.end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        self.download_path = os.path.normpath(path)
-        self.__check_path(self.download_path)
+    def __init__(
+        self, var_type: str, start_date: str, end_date: str, path: str
+    ) -> None:
+        self.var_type = var_type
+        self.__imd_obj = IMD(self.var_type)
+        self.start_date, self.end_date = self.__imd_obj._check_dates(
+            datetime.strptime(start_date, "%Y-%m-%d"),
+            datetime.strptime(end_date, "%Y-%m-%d"),
+        )
+        self.download_path = self.__imd_obj._check_path(path)
         self.skipped_download = []
         self.failed_conversion = []
-
-    def __check_path(self, path: str) -> None:
-        if not os.path.isdir(path):
-            raise OSError(f"{path} does not exist.")
 
     def __download(self):
         # check dates in IMD Availability
         # start date == or > end date
         pass
+
 
 # def get_data(start_date: str, end_date: str, path):
 #     """ change str dates into datetime objs
